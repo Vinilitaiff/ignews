51bef30f440c69d495d09f19492340de03940369
Vinicius <vinilitaiff@gmail.com>
2021-05-24 14:19:24 -0400

    componentes: Header, SignButton e SubsButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,42 @@
+import {GetServerSideProps} from 'next'
 import Head from 'next/head';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ig.News</title>
+        <title>Home | Ig.News</title>
       </Head>
-      <h1>      
-        Hello World
-      </h1>
+      <main className={styles.contentConntainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.94 month</span>
+          </p> 
+          <SubscribeButton/>
+        </section>
+        
+        <img src="images/avatar.svg" alt="Girl Coding"/>
+      </main>      
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () =>{
+  const price = await stripe.prices.retrieve('price_1IuhTrItCXvsRqgk2UsOHjbE',{
+    expand:['product']
+  })
+
+  return{
+    props: {
+      nome: 'Vinicius'
+    }
+  }
 }
